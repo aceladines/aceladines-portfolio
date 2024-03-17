fd843cec9ecc62ddd36e2dcab8415d251399879a
Ace Ladines <ajladines01@gmail.com>
2024-03-17 22:26:59 +0800

    [FEATURE] - Added skills data.

---
+++ data/skills.ts
@@ -0,0 +1,60 @@
+import {
+  SiTypescript,
+  SiJavascript,
+  SiExpress,
+  SiNextdotjs,
+  SiMongodb,
+  SiTailwindcss,
+} from "react-icons/si";
+import { FaNodeJs, FaJava, FaGitAlt } from "react-icons/fa";
+import { BiLogoSpringBoot } from "react-icons/bi";
+import { TbFileTypeSql } from "react-icons/tb";
+
+const skills = [
+  {
+    techImage: SiJavascript,
+    techName: "JavaScript",
+  },
+  {
+    techImage: SiTypescript,
+    techName: "Typescript",
+  },
+  {
+    techImage: FaNodeJs,
+    techName: "Node.js",
+  },
+  {
+    techImage: SiExpress,
+    techName: "Express.js",
+  },
+  {
+    techImage: FaJava,
+    techName: "Java",
+  },
+  {
+    techImage: BiLogoSpringBoot,
+    techName: "Spring Boot",
+  },
+  {
+    techImage: FaGitAlt,
+    techName: "Git",
+  },
+  {
+    techImage: SiNextdotjs,
+    techName: "Next.js",
+  },
+  {
+    techImage: TbFileTypeSql,
+    techName: "SQL",
+  },
+  {
+    techImage: SiMongodb,
+    techName: "NoSQL",
+  },
+  {
+    techImage: SiTailwindcss,
+    techName: "Tailwind",
+  },
+];
+
+export default skills;
