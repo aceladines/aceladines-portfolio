2dd4d121c45127a51600cce60afca5825e39107b
Ace Ladines <ajladines01@gmail.com>
2024-03-18 23:36:22 +0800

    [FEATURE] - Initialized the footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="mx-auto w-[90vw] border-t-[1px] border-gray-400 flex flex-col items-center mb-4">
+      <h1 className="font-medium text-sm text-gray-700 mt-4">
+        Made with <span className="bg-blue-500 p-1 rounded-full">🫰</span>
+      </h1>
+    </div>
+  );
+}
