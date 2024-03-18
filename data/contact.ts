0f3a9229ed14fdf4f7494f561d1d8ea458dd49ad
Ace Ladines <ajladines01@gmail.com>
2024-03-18 23:36:43 +0800

    [FEATURE] - Initialized the data for contact

---
+++ data/contact.ts
@@ -0,0 +1,22 @@
+const contact = [
+  {
+    contactImage: "",
+    contactType: "Email",
+    contact: "ajladines01@gmail.com",
+    contactLink: "",
+  },
+  {
+    contactImage: "",
+    contactType: "Github",
+    contact: "aceladines",
+    contactLink: "",
+  },
+  {
+    contactImage: "",
+    contactType: "LinkedIn",
+    contact: "Acemon Jassene Ladines",
+    contactLink: "",
+  },
+];
+
+export default contact;
