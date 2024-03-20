28a9448be5fec23ed50f51b116e89cccf5ac6440
Ace Ladines <ajladines01@gmail.com>
2024-03-21 00:37:47 +0800

    [ADD] - Projects data for consumption

---
+++ data/projects.ts
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    image: "/201Jacket.jpeg",
+    projectTitle: "201Jacket",
+    projectDetail:
+      "An online platform where students can seamlessly upload their documents, like essential paperwork such as birth certificate, form 137-138, etc.",
+  },
+  {
+    image: "/cas.jpeg",
+    projectTitle: "Counseling Appointment System",
+    projectDetail:
+      "Developed to revolutionize mental health care within the university by offering user-friendly platform for scheduling appointments.",
+  },
+  {
+    image: "/eams.png",
+    projectTitle: "Educational Assistance Management System",
+    projectDetail:
+      "Web application that allows student to easily find available scholarships offered within the university.",
+  },
+];
+
+export default projects;
